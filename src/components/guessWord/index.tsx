c2bf92ab248479874afc90a225dc6d1b514435fb
gharr86 <gharr86@gmail.com>
2022-02-17 15:42:55 -0300

    refactored guess background

--- src/components/guessWord/index.tsx
+++ src/components/guessWord/index.tsx
@@ -1,12 +1,19 @@
 import React, { ReactElement } from 'react';
 import { nanoid } from 'nanoid';
 
-import { GuessLetter, GuessWordProps } from '../../types';
+import { GuessStatus, GuessLetter, GuessWordProps } from '../../types';
 
-const backgrounds = {
-  correct: '#0b8c34',
-  'wrong-position': '#946f00',
-  incorrect: '#5e5e5e',
+const getBackgroundColor = (status: GuessStatus): string => {
+  switch (status) {
+    case 'correct':
+      return '#0b8c34';
+    case 'wrong-position':
+      return '#946f00';
+    case 'incorrect':
+      return '#5e5e5e';
+    default:
+      return '';
+  }
 };
 
 const GuessWord = ({ letters }: GuessWordProps): JSX.Element => {
@@ -16,7 +23,7 @@ const GuessWord = ({ letters }: GuessWordProps): JSX.Element => {
         letters.map((guessLetter: GuessLetter): ReactElement => (
           <div
             className="guess-word__letter"
-            style={{ background: backgrounds[guessLetter.status] }}
+            style={{ background: getBackgroundColor(guessLetter.status) }}
             key={nanoid()}
           >
             {guessLetter.value}
