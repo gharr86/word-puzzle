0dbdb46b5d0b7d9872d2eb57da6fa10876562c3b
gharr86 <gharr86@gmail.com>
2022-03-04 11:42:20 -0300

    add tests for gameOverPrompt

---
+++ src/components/gameOverPrompt/gameOverPrompt.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { fireEvent, render, RenderResult, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import GameOverPrompt from '.';
+import { GameOverPromptProps } from '../../types';
+
+const basicProps: GameOverPromptProps = {
+  onClickOutside: jest.fn(),
+  word: 'VIAJE',
+  didWin: false,
+};
+
+const renderGameOverPrompt = (newProps: {} = {}): RenderResult => {
+  const props = {
+    ...basicProps,
+    ...newProps,
+  };
+
+  return render(<GameOverPrompt {...props} />);
+};
+
+describe('<GameOverPrompt />', () => {
+  test('basic render', () => {
+    renderGameOverPrompt();
+  });
+
+  test('when didWin is false, loose message is rendered and word is shown', () => {
+    renderGameOverPrompt();
+
+    expect(screen.getByText(/perdiste/i)).toBeInTheDocument();
+    expect(screen.getByText(basicProps.word)).toBeInTheDocument();
+  });
+
+  test('when didWin is true, win message is rendered and word is not shown', () => {
+    renderGameOverPrompt({ didWin: true });
+
+    expect(screen.getByText(/ganaste/i)).toBeInTheDocument();
+    expect(screen.queryByText(basicProps.word)).not.toBeInTheDocument();
+  });
+
+  test('when background is clicked, onClickOutside prop is called', () => {
+    renderGameOverPrompt();
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(basicProps.onClickOutside).toHaveBeenCalled();
+  });
+});
